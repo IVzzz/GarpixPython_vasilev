6f9b3c38c8e0b83966c4d2775a2f8ffd239f26d1
Igor Vasilev <igor.vasiliev.n@gmail.com>
2022-08-11 01:44:41 +0300

    add task1.1

---
+++ Тема 1/Задание 1.1.py
@@ -0,0 +1,5 @@
+surname = input("Введите фамилию")
+name = input("Введите имя")
+secname = input("Введите отчество")
+addres = input("Введите адрес")
+print(f"Здравствуйте, {surname.capitalize()} {name.capitalize()} {secname.capitalize()}. Ваш заказ отправлен по адресу {addres.capitalize()}")

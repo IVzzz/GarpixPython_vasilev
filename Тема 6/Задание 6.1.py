8a105fd597ea3694339d888a36b4a7b1d0f8503c
Igor Vasilev <igor.vasiliev.n@gmail.com>
2022-08-28 22:11:29 +0300

    new task add 6

---
+++ Тема 6/Задание 6.1.py
@@ -0,0 +1,17 @@
+class Parallelepiped:
+    def __init__(self, width, length, height):
+        self.width = width
+        self.length = length
+        self.height = height
+
+    def countBaseSquare(self):
+        return self.width * self.length
+
+    def countWidthSideSquare(self):
+        return self.height * self.width
+
+    def countLengthSideSquare(self):
+        return self.height * self.length
+
+    def countVolume(self):
+        return self.width * self.length * self.height

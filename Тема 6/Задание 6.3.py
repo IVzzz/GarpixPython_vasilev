2ef0d47d523a5915aad4ac06e4a154717c78379b
Igor Vasilev <igor.vasiliev.n@gmail.com>
2022-08-28 22:26:04 +0300

    new task add 6

--- Тема 6/Задание 6.3.py
+++ Тема 6/Задание 6.3.py
@@ -1,1 +1,7 @@
-
+class Nikola:
+    def __init__(self, name, age):
+        self.age = age
+        if name != "Николай":
+            self.name = f'Я не {name}, а Николай'
+        else:
+            self.name = name

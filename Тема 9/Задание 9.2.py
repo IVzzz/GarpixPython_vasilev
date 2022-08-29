c1a55ab8bd4d0685935abe3cf5653f83940218f3
Igor Vasilev <igor.vasiliev.n@gmail.com>
2022-08-29 20:55:18 +0300

    new task add 9

--- Тема 9/Задание 9.2.py
+++ Тема 9/Задание 9.2.py
@@ -1,6 +1,15 @@
 import requests
 
-
+# не очень понял, в каком виде передается url. Это вариант, когда дают просто url и словарь с параметрами
 def send_request(url, params):
     response = requests.get(url, params=params)
     return response
+
+# это вариант, когда дан список параметров и url в виде smt.com/?param1=@&param2=@&param3=@&...
+def send_request1(url, params):
+    p = 0
+    while url.count("@"):
+        url = url.replace("@", params[p], 1)
+        p += 1
+    response = requests.get(url)
+    return response.url
